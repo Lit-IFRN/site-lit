88324388d45cc7ccf055f80e2349441341313c71
José Eduardo <josee.edumont@gmail.com>
2020-12-18 18:29:29 -0300

    added dinamically querying for thumbs on projects page

--- src/pages/projetos-dinamicos.js
+++ src/pages/projetos-dinamicos.js
@@ -1,16 +1,35 @@
 import LinesEllipsis from 'react-lines-ellipsis';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import Layout from '../components/layout/Layout';
 import Card from '../components/Card';
 import useProjects from '../hooks/useProjects';
 
-const CardAlo = ({ title, description = 'Descrição', image_name, url = '' }) => (
-  <div className="w-12/12 sm:w-6/12 lg:w-4/12 ">
-    <Card className="m-3 shadow-lg" noPadding>
-      <img className="sm:mb-4 rounded-t-lg object-cover w-100" src={image_name} />
+export const query = graphql`
+  query {
+    allFile(filter: { relativeDirectory: { regex: "/images/i" } }) {
+      edges {
+        node {
+          name
+          childImageSharp {
+            fluid(maxWidth: 1800, maxHeight: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
+const ProjectCard = ({ title, description = 'Descrição', image, url = '' }) => (
+  <div className="w-12/12 sm:w-6/12 lg:w-4/12 ">
+    <Card className="m-3 shadow-lg">
       <div className="p-1 sm:p-3">
-        <h1 className="text-xl text-center font-bold sm:mb-4">{title}</h1>
+        <Img fluid={image} className="sm:mb-4 rounded-t-lg" alt={title} />
+        <Link to={url}>
+          <h1 className="text-xl text-center font-bold sm:mb-4">{title}</h1>
+        </Link>
 
         <LinesEllipsis
           component="p"
@@ -26,8 +45,15 @@ const CardAlo = ({ title, description = 'Descrição', image_name, url = '' }) =
   </div>
 );
 
-const Projects = () => {
+const findProjectImage = (images, imageName) => {
+  const { node } = images.find(image => image.node.name === imageName);
+  return node.childImageSharp.fluid;
+};
+
+const Projects = ({ data: { allFile } }) => {
+  const images = allFile.edges;
   const projects = useProjects();
+
   return (
     <Layout>
       <div className="mx-auto container px-8">
@@ -36,11 +62,11 @@ const Projects = () => {
         </h1>
         <div className="flex items-center flex-col sm:flex-row flex-wrap">
           {projects.map(project => (
-            <CardAlo
+            <ProjectCard
               key={project.slug}
               title={project.title}
               description={project.description}
-              image_name={project.image_name}
+              image={findProjectImage(images, project.image_name)}
               url={project.slug}
             />
           ))}
